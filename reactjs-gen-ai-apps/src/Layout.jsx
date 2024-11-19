db0d869e45dda93dc5a95f2cf0fa65a11949fded
Yuguo LI <hugo.li@landservices.com.au>
2024-11-19 23:51:26 +1030

    remove the nav MultiLLM

--- reactjs-gen-ai-apps/src/Layout.jsx
+++ reactjs-gen-ai-apps/src/Layout.jsx
@@ -29,14 +29,14 @@ export default () => {
                     }
                     items={
                         [
-                            {
-                                type: "section", text: "Chat With Amazon Bedrock Multimodal", items: [
-                                    // { type: "link", href: "/llm", text: "Chat Q&A" },
-                                    // { type: "link", href: "/chat", text: "Chat with Memory" }, 
-                                    { type: "link", href: "/multimodal", text: "Multimodal Chatbot" },
+                            // {
+                            //     type: "section", text: "Chat With Amazon Bedrock Multimodal", items: [
+                            //         // { type: "link", href: "/llm", text: "Chat Q&A" },
+                            //         // { type: "link", href: "/chat", text: "Chat with Memory" }, 
+                            //         { type: "link", href: "/multimodal", text: "Multimodal Chatbot" },
 
-                                ]
-                            },
+                            //     ]
+                            // },
                             {
                                 type: "section", text: "System Prompts", items: [
                                     { type: "link", href: "/prompt", text: "Prompts" }
