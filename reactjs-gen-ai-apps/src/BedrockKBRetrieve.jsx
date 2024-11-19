edfc6c123a1bc041f773934d5d3765ef2a366eec
Yuguo LI <hugo.li@landservices.com.au>
2024-11-19 20:23:29 +1030

    commont out score and topK component

--- reactjs-gen-ai-apps/src/BedrockKBRetrieve.jsx
+++ reactjs-gen-ai-apps/src/BedrockKBRetrieve.jsx
@@ -70,19 +70,18 @@ export default () => {
                 <BedrockKBLoader ref={childRef} key={1} />
                 <FMPicker ref={childRef2} multimodal={true} key={3} />
 
-                <FormField label="MinScore ">
+                {/* <FormField label="MinScore ">
                     <Input type="number" inputMode="numeric" 
                         value={scoreValue.toString()}
                         onChange={({ detail }) => {
                             console.log("MinScore change: "+detail)
                             // setScoreValue(Number(detail.value))
-
                         }}
                         key={4}
                         />
-                </FormField>
+                </FormField> */}
 
-                <FormField label="Top K">
+                {/* <FormField label="Top K">
                     <Input type="number" inputMode="numeric" 
                         value={topKValue.toString()}
                         onChange={({ detail }) => {
@@ -91,7 +90,7 @@ export default () => {
                         }}
                         key={5}
                         />
-                </FormField>
+                </FormField> */}
                 <Box data-id="chat-window">
                     {
                         messages.length ?
