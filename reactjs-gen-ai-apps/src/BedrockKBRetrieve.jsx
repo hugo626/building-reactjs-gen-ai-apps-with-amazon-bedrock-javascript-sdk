b0f3f9c4e65f47a331bdd92185c19cdebea637c7
Yuguo LI <hugo.li@landservices.com.au>
2024-11-19 22:03:53 +1030

    include the slider component

--- reactjs-gen-ai-apps/src/BedrockKBRetrieve.jsx
+++ reactjs-gen-ai-apps/src/BedrockKBRetrieve.jsx
@@ -1,6 +1,6 @@
 import {useState, useRef } from "react"
 
-import { Box, Spinner, Header, Container, SpaceBetween, Textarea, Button, Input, FormField } from "@cloudscape-design/components"
+import { Box, Spinner, Header, Container, SpaceBetween, Textarea, Button, Input, FormField, Slider } from "@cloudscape-design/components"
 import MessageList from "./MessageList"
 import BedrockKBLoader from "./BedrockKBLoader";
 import { getBedrockKnowledgeBaseRetriever, retrieveBedrockKnowledgeBase } from "./llmLib"
