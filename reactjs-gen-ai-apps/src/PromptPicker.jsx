479614043c9e9c398953a07723cfb746765a1cc7
Yuguo LI <hugo.li@landservices.com.au>
2024-11-21 10:17:04 +1030

    Add the default prompt for the Retrieve => LLM page

--- reactjs-gen-ai-apps/src/PromptPicker.jsx
+++ reactjs-gen-ai-apps/src/PromptPicker.jsx
@@ -17,6 +17,11 @@ export default forwardRef(({ }, ref) => {
 
     const getList = async () => {
         let list = await fetchByValue("listPrompts")
+        list.unshift(
+            { label: "Default Prompt for Retrieve => LLM", 
+              value: "Use the following pieces of documents to answer the question at the end. If you don't know the answer, just say that you don't know, don't try to make up an answer. Provide sources (in the <source> tags within your response)" 
+            }
+        )
         let first = list[0]
         setPrompts(list)
         if (first) {
